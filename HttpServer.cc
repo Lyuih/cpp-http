b4dd4caffd3c8ce0d2eb01953f2167df3fa9c3f5
Lyuih <hua642948984@outlook.com>
2026-03-20 16:21:09 +0800

    实现本地资源的展示

--- HttpServer.cc
+++ HttpServer.cc
@@ -5,7 +5,7 @@ HttpServer::HttpServer(const int port, const int thread_num)
       thread_num_(thread_num)
 {
     TcpServer::getInstance();
-    TcpServer::getInstance().init(port_, thread_num_,std::bind(&HttpServer::onMessage,this,std::placeholders::_1));
+    TcpServer::getInstance().init(port_, thread_num_, std::bind(&HttpServer::onMessage, this, std::placeholders::_1));
     TcpServer::getInstance().createSocket();
 }
 
@@ -17,21 +17,21 @@ void HttpServer::loop()
 // 核心业务处理入口
 void HttpServer::onMessage(int fd)
 {
-    //1.读取数据
+    // 1.读取数据
     Buffer buffer;
     int err = 0;
-    if(buffer.readFd(fd,&err) <=0)
+    if (buffer.readFd(fd, &err) <= 0)
     {
         LOG_WARNING("连接断开或是读取数据错误");
         ::close(fd);
         return;
     }
-    //2.解析数据
+    // 2.解析数据
     HttpRequest http_request;
-    if(http_request.parse(buffer))
+    if (http_request.parse(buffer))
     {
-        //3.执行业务逻辑
-        handleRequest(fd,http_request);
+        // 3.执行业务逻辑
+        handleRequest(fd, http_request);
     }
     else
     {
@@ -42,18 +42,68 @@ void HttpServer::onMessage(int fd)
 // 根据请求路径分发逻辑
 void HttpServer::handleRequest(int fd, HttpRequest &req)
 {
-    HttpResponse http_response;
-    if(req.path() == "/index.html" || req.path() == "/" || req.path() == "")
+    // HttpResponse http_response;
+    // if (req.path() == "/index.html" || req.path() == "/" || req.path() == "")
+    // {
+    //     http_response.setStatus(200);
+    //     http_response.setBody("<h1>Welcome to My Server!</h1>");
+    // }
+    // else
+    // {
+    //     http_response.setStatus(404);
+    //     http_response.setBody("<h1>404 Not Found</h1>");
+    // }
+    // // 返回数据
+    // std::string data = http_response.toString();
+    HttpResponse http_response = handleStaticFile(req.path());
+    std::string data = http_response.toString();
+    TcpServer::getInstance().sendMsg(fd, data);
+}
+
+// 处理静态问文件
+HttpResponse HttpServer::handleStaticFile(const std::string &path)
+{
+    HttpResponse res;
+    // 1.查看是否是本地资源
+    std::string full_path = "./wwwroot";
+    if (path == "/" || path == "")
     {
-        http_response.setStatus(200);
-        http_response.setBody("<h1>Welcome to My Server!</h1>");
+        full_path += "/index.html";
     }
     else
     {
-        http_response.setStatus(404);
-        http_response.setBody("<h1>404 Not Found</h1>");
+        full_path += path;
     }
-    //返回数据
-    std::string data = http_response.toString();
-    TcpServer::getInstance().sendMsg(fd,data);
+    // 2.尝试以二进制方式打开文件
+    std::fstream file(full_path, std::ios::binary | std::ios::in);
+    if (!file.is_open())
+    {
+        // 不存在
+        LOG_DEBUG("文件不存在，返回404:%s", full_path.c_str());
+        res.setStatus(404);
+        res.setBody("<h>404</h>");
+        return res;
+    }
+
+    // 3.识别后缀并设置content-type
+    auto pos = path.find_last_of('.');
+    std::string suffix;
+    if (pos != std::string::npos)
+    {
+        suffix = path.substr(pos);
+    }
+    if (SUFFIX_TYPE.count(suffix))
+    {
+        res.setContentType(SUFFIX_TYPE.at(suffix));
+    }
+    else
+    {
+        // 未知类型使用二进制流
+        res.setContentType("application/octet-stream");
+    }
+    std::stringstream ss;
+    ss << file.rdbuf();
+    res.setBody(ss.str());
+    res.setStatus(200);
+    return res;
 }
--- HttpServer.h
+++ HttpServer.h
@@ -12,6 +12,9 @@
 
 #ifndef HTTPSERVER_H
 #define HTTPSERVER_H
+
+#include <fstream>
+#include <sstream>
 #include "TcpServer.h"
 #include "HttpRequest.h"
 #include "HttpResponse.h"
@@ -25,6 +28,28 @@
 #define DEFAULT_PORT 8888
 #define THREAD_NUM 10
 
+static const std::unordered_map<std::string, std::string> SUFFIX_TYPE = {
+    {".html", "text/html"},
+    {".xml", "text/xml"},
+    {".xhtml", "application/xhtml+xml"},
+    {".txt", "text/plain"},
+    {".rtf", "application/rtf"},
+    {".pdf", "application/pdf"},
+    {".word", "application/msword"},
+    {".png", "image/png"},
+    {".gif", "image/gif"},
+    {".jpg", "image/jpeg"},
+    {".jpeg", "image/jpeg"},
+    {".au", "audio/basic"},
+    {".mpeg", "video/mpeg"},
+    {".mpg", "video/mpeg"},
+    {".avi", "video/x-msvideo"},
+    {".gz", "application/x-gzip"},
+    {".tar", "application/x-tar"},
+    {".css", "text/css"},
+    {".js", "text/javascript"},
+};
+
 class HttpServer
 {
 public:
@@ -36,6 +61,8 @@ public:
     void onMessage(int fd);
 
 private:
+    // 处理静态问文件
+    HttpResponse handleStaticFile(const std::string& path);
     // 根据请求路径分发逻辑
     void handleRequest(int fd, HttpRequest &req);
 
